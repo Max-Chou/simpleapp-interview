8970e86e290c6dae0fbef766175b47c43189bf28
Chi-Hui Chou <afai97202013@gmail.com>
2020-03-12 22:18:25 +0800

    feat: add user registration and send confirm email

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ class Config:
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
         ['true', 'on', '1']
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or "c560e4b4fd2dd9"
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or "00b389e7c30bf1"
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or "flasky"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
