47f3cc2969d5fa339109250892b883081fe9ea69
Chi-Hui Chou <afai97202013@gmail.com>
2020-04-05 15:55:15 +0800

    feat: add api

--- flasky/app/main/errors.py
+++ flasky/app/main/errors.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from . import main
 
 
@@ -10,3 +10,13 @@ def page_not_found(e):
 @main.app_errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+@main.app_errorhandler(403)
+def forbidden(e):
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'forbidden'})
+        response.status_code = 403
+        return response
+    return render_template('403.html'), 403
